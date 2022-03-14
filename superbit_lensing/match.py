7c7f8aeaa1a775ad1baa35d464752eab2e2e9f62
Spencer Everett <spencerweverett@gmail.com>
2022-03-14 12:35:46 -0700

    refactored to have a base matcher that does not have a corresponding truth.

--- superbit_lensing/match.py
+++ superbit_lensing/match.py
@@ -7,35 +7,30 @@ import esutil.htm as htm
 from astropy.table import Table
 
 class MatchedCatalog(object):
-    '''
-    Could have a matched class not dependent on
-    a truth catalog, but haven't had a use yet
-    '''
-    pass
 
-class MatchedTruthCatalog(MatchedCatalog):
-
-    def __init__(self, true_file, meas_file,
-                 true_ratag='ra', true_dectag='dec',
-                 meas_ratag='ra', meas_dectag='dec',
+    def __init__(self, cat1_file, cat2_file,
+                 cat1_ratag='ra', cat1_dectag='dec',
+                 cat2_ratag='ra', cat2_dectag='dec',
+                 cat1_hdu=1, cat2_hdu=1,
                  match_radius=1.0/3600, depth=14):
         '''
         match_radius is in deg, same as htm
         '''
 
-        self.true_file = true_file
-        self.meas_file = meas_file
+        self.cat1_file = cat1_file
+        self.cat2_file = cat2_file
 
-        self.true_ratag  = true_ratag
-        self.meas_ratag  = meas_ratag
-        self.true_dectag = true_dectag
-        self.meas_dectag = meas_dectag
+        self.cat1_ratag  = cat1_ratag
+        self.cat2_ratag  = cat2_ratag
+        self.cat1_dectag = cat1_dectag
+        self.cat2_dectag = cat2_dectag
 
         self.match_radius = match_radius
         self.depth = depth
 
-        self.meas = None
-        self.true = None
+        self.cat2 = None
+        self.cat1 = None
+        self.cat = None # matched catalog
         self.Nobjs = 0
 
         self._match()
@@ -43,35 +38,70 @@ class MatchedTruthCatalog(MatchedCatalog):
         return
 
     def _match(self):
-        true_cat, meas_cat = self._load_cats()
+        cat1_cat, cat2_cat = self._load_cats()
 
         h = htm.HTM(self.depth)
 
         self.matcher = htm.Matcher(
             depth=self.depth,
-            ra=true_cat[self.true_ratag],
-            dec=true_cat[self.true_dectag]
+            ra=cat1_cat[self.cat1_ratag],
+            dec=cat1_cat[self.cat1_dectag]
             )
 
         id_m, id_t, dist = self.matcher.match(
-            ra=meas_cat[self.meas_ratag],
-            dec=meas_cat[self.meas_dectag],
+            ra=cat2_cat[self.cat2_ratag],
+            dec=cat2_cat[self.cat2_dectag],
             radius=self.match_radius
             )
 
-        self.true = true_cat[id_t]
-        self.meas = meas_cat[id_m]
-        self.meas['separation'] = dist
+        self.cat1 = cat1_cat[id_t]
+        self.cat2 = cat2_cat[id_m]
+        self.cat2['separation'] = dist
         self.dist = dist
 
-        assert len(self.true) == len(self.meas)
+        assert len(self.cat1) == len(self.cat2)
 
-        self.Nobjs = len(self.true)
+        self.cat = hstack([self.cat1, self.cat2])
+
+        self.Nobjs = len(self.cat)
 
         return
 
     def _load_cats(self):
-        true_cat = Table.read(self.true_file)
-        meas_cat = Table.read(self.meas_file)
+        cat1_cat = Table.read(self.cat1_file)
+        cat2_cat = Table.read(self.cat2_file)
 
-        return true_cat, meas_cat
+        return cat1_cat, cat2_cat
+
+class MatchedTruthCatalog(MatchedCatalog):
+    '''
+    Same as MatchedCatalog, where one cat holds
+    truth information
+
+    Must pass truth cat first
+    '''
+
+    def __init__(self, truth_file, meas_file, **kwargs):
+
+        cat1, cat2 = truth_file, meas_file
+        super(MatchedTruthCatalog, self).__init__(
+            cat1, cat2, **kwargs
+            )
+
+        return
+
+    @property
+    def truth_file(self):
+        return self.cat1_file
+
+    @property
+    def meas_file(self):
+        return self.cat2_file
+
+    @property
+    def truth(self):
+        return self.cat1
+
+    @property
+    def meas(self):
+        return self.cat2
