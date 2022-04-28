a3cf90429670a4fc421a89fc4c4a600d24eee21e
Spencer Everett <spencerweverett@gmail.com>
2022-04-28 13:55:37 +0200

    Updated yaml loader to correctly handle scientific notation

--- superbit_lensing/utils.py
+++ superbit_lensing/utils.py
@@ -2,6 +2,7 @@ import logging
 import os
 import sys
 import yaml
+import re
 from astropy.table import Table
 import superbit_lensing as sb
 import numpy as np
@@ -107,8 +108,27 @@ def setup_logger(logfile, logdir=None):
     return logger.log
 
 def read_yaml(yaml_file):
+    '''
+    current package has a problem reading scientific notation as
+    floats; see
+    https://stackoverflow.com/questions/30458977/yaml-loads-5e-6-as-string-and-not-a-number
+    '''
+
+    loader = yaml.SafeLoader
+    loader.add_implicit_resolver(
+        u'tag:yaml.org,2002:float',
+        re.compile(u'''^(?:
+        [-+]?(?:[0-9][0-9_]*)\\.[0-9_]*(?:[eE][-+]?[0-9]+)?
+        |[-+]?(?:[0-9][0-9_]*)(?:[eE][-+]?[0-9]+)
+        |\\.[0-9_]+(?:[eE][-+][0-9]+)?
+        |[-+]?[0-9][0-9_]*(?::[0-5]?[0-9])+\\.[0-9_]*
+        |[-+]?\\.(?:inf|Inf|INF)
+        |\\.(?:nan|NaN|NAN))$''', re.X),
+        list(u'-+0123456789.'))
+
     with open(yaml_file, 'r') as stream:
-        return yaml.safe_load(stream)
+        # return yaml.safe_load(stream) # see above issue
+        return yaml.load(stream, Loader=loader)
 
 def write_yaml(yaml_dict, yaml_outfile):
     with open(yaml_outfile, 'w') as yaml_file:
