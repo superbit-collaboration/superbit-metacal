73656e85c6c4c3e35f040318dd7ecc2c00a0ea45
mcclearyj <jmac.ftw@gmail.com>
2022-07-20 19:21:02 -0400

    working branch to make metacal work on background-only galaxies

--- superbit_lensing/shear_profiles/annular_jmac.py
+++ superbit_lensing/shear_profiles/annular_jmac.py
@@ -334,7 +334,7 @@ class Annular(object):
 
         return nfw_tab
 
-    def _nfw_resample_redshift(self, gal_redshifts, outdir='.', nfactor=100,
+    def _nfw_resample_redshift(self, gal_redshifts, outdir='.', nfactor=10,
                                overwrite=False):
         '''
         Subsample theoretical galaxy redshift distribution to match redshift
