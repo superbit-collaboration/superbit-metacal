2d577d944608b599de76d2159c01f4d08dda9225
mcclearyj <jmac.ftw@gmail.com>
2022-08-10 21:29:14 -0400

    Undo mistaken commit of pipe.py

--- superbit_lensing/pipe.py
+++ superbit_lensing/pipe.py
@@ -636,9 +636,8 @@ def make_test_config(config_file='pipe_test.yaml', outdir=None, clobber=False):
                                                    run_name=run_name)
 
         # dummy truth
-        #nfw_file = os.path.join(
-        #    utils.BASE_DIR, 'runs/truth/nfw_truth_files/nfw_cl_m2.2e15_z0.44.fits')
-        nfw_file = '/users/jmcclear/data/superbit/mock-data-forecasting/nfw_truth/nfw_cl_m5.8e14_z0.075.fits'
+        nfw_file = os.path.join(
+            utils.BASE_DIR, 'runs/truth/nfw_truth_files/nfw_cl_m2.2e15_z0.44.fits')
 
         overwrite = True
         with open(filename, 'w') as f:
