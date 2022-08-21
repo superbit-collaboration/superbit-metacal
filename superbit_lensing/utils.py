1aa2788e72d9f8ed202e67807220ce8dc81bdd05
Spencer Everett <spencerweverett@gmail.com>
2022-08-21 11:25:11 -0700

    Allow for specified `seed_bounds` in `generate_seeds()`, as mcal is more stringent in range than the other modules.

--- superbit_lensing/utils.py
+++ superbit_lensing/utils.py
@@ -143,7 +143,7 @@ def write_yaml(yaml_dict, yaml_outfile):
 
     return
 
-def generate_seeds(Nseeds, master_seed=None):
+def generate_seeds(Nseeds, master_seed=None, seed_bounds=(0, 2**32-1)):
     '''
     generate a set of safe, independent seeds given a master seed
 
@@ -151,10 +151,20 @@ def generate_seeds(Nseeds, master_seed=None):
         The number of desired independent seeds
     master_seed: int
         A seed that initializes the SeedSequence, if desired
+    seed_bounds: tuple of ints
+        The min & max values for the seeds to be sampled from
     '''
 
     if (not isinstance(Nseeds, int)) or (Nseeds < 1):
         raise ValueError('Nseeds must be a positive int!')
+
+    for b in seed_bounds:
+        if not isinstance(b, int):
+            raise TypeError('seed_bounds must be a tuple of ints!')
+        if seed_bounds[0] < 0:
+            raise ValueError('seed_bounds must be positive!')
+        if seed_bounds[1] < seed_bounds[0]:
+            raise ValueError('seed_bounds values must be monotonic!')
 
     if master_seed is None:
         # local time in microseconds
@@ -166,7 +176,7 @@ def generate_seeds(Nseeds, master_seed=None):
 
     seeds = []
     for k in range(Nseeds):
-        val = int(streams[k].random()*1e16)
+        val = int(streams[k].integers(seed_bounds[0], seed_bounds[1]))
         seeds.append(val)
 
     return seeds
