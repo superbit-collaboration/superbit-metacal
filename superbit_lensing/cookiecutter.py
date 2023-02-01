1b8c48bf5cc4a6d1aa1e73862312821a38c91867
emhuff <emhuff@gmail.com>
2023-02-01 11:59:22 -0800

    first draft of cookie cutout retrieval method

--- superbit_lensing/cookiecutter.py
+++ superbit_lensing/cookiecutter.py
@@ -8,7 +8,7 @@ from astropy.coordinates import SkyCoord
 import astropy.units as u
 
 from superbit_lensing import utils
-
+import glob
 
 
 def intersecting_slices(big_array_shape,small_array_shape,position):
@@ -183,9 +183,11 @@ class CookieCutter(object):
         self.config = config
         if (cookiecutterfile is None) and (config is not None):
             # Initialize from config
+            self._cookieCutterFile = Path(config['global output path']) / Path( config['output filename'] )
             pass
         elif (cookiecutterfile is not None) and (config is None):
             # Initialize from file
+            self._cookieCutterFile = Path(cookiecutterfile)
             pass
         else:
             raise ValueError("You should pass only a cookie cutout file or a config, not both.")
@@ -249,11 +251,9 @@ class CookieCutter(object):
         boxsize: sring such that catalog call above works
         images: list of dictionaries, each containing image/weight/mask/background file+extension info.
         outfile: where to put the output.
-
-
+        
 
         TODO: CONFIRM THAT THE OBJECT DATA TABLE IS INITIALIZED WITH SUFFICIENT LENGTH TO HOLD THE IMAGE FILENAME.
-        TODO: Ensure that the first non-empty fits extension is the metadata table.
         '''
         
         # We get a list of images from the config file.
@@ -275,7 +275,9 @@ class CookieCutter(object):
         object_info_table = np.empty(catalog.size * len(images), dtype = [('object id',int),
                                                                           ('imagefile','S64'),
                                                                           ('startpos',int),
-                                                                          ('skylevel',float]))
+                                                                          ('endpos',int),
+                                                                          ('skylevel',float),
+                                                                          ('extnumber',int)])
         info_index = 0
         
         # The first non-empty extension should be the metadata table.
@@ -304,7 +306,7 @@ class CookieCutter(object):
             # Store each image's cutouts in a new extension.
             # Because the image and background have different datatypes to the mask,
             # we need two different extensions.
-            fits.create_image_hdu(img=None,dtype='i2',dims=dims,extname=f'SCI{image_index}',header=imageHDR)
+            fits.create_image_hdu(img=None,dtype='i2',dims=dims,extname=f'IMAGE{image_index}',header=imageHDR)
 
             
             fits.create_image_hdu(img=None,dtype='i1',dims=dims,extname=f'MASK{image_index}')
@@ -323,9 +325,9 @@ class CookieCutter(object):
                 science_output = image_cutout.flatten() #np.vstack([image_cutout.flatten(order='C'),sky_cutout.flatten(order='C')])
 
                 if objindx == 0:
-                    fits[f'SCI{image_index}'].write(science_output,start=[0,pixels_written],header=imageHDR)
+                    fits[f'IMAGE{image_index}'].write(science_output,start=[0,pixels_written],header=imageHDR)
                 else:
-                    fits[f'SCI{image_index}'].write(science_output,start=[0,pixels_written])
+                    fits[f'IMAGE{image_index}'].write(science_output,start=[0,pixels_written])
                 
 
                 if imageObj.sky is not None:
@@ -347,11 +349,76 @@ class CookieCutter(object):
 
                 object_info_table[info_index]['object id'] = iobj['id']
                 object_info_table[info_index]['imagefile'] = image['imagefile']
-                object_info_table[info_index]['startpos'] = npix_sci_written # This is how we look up objects later.
+                object_info_table[info_index]['startpos'] = npix_sci_written # This is how we look up object positions to read later.
                 object_info_table[info_index]['background'] = sky_level
+                object_info_table[info_index]['extension'] = image_index
+
                 
                 npix_sci_written = npix_sci_written + sky_cutout.size
                 info_index = info_index+1
         fits['META'].write(object_info_table)
+
+        # Finally, populate the object info table if we need it for later.
+        self._object_info_table = object_info_table
         fits.close()
+
+    @property
+    def objectInfoTable(self):
+        if self._object_info_table is None:
+            self._object_info_table = fitsio.read(self._cookieCutterFile,ext='META')
+        return self._object_info_table
+            
+            
+
+    def _getOneCutout(self,objectid, extnumber= None, filename = None, cutoutType = 'IMAGE'):
+        '''
+        Request a single cutout of one object from one image.
+        Must always provide:
+        objectid:  unique object id, as listed in the metadata table
+        --------------
+        Must always provide ONLY one of either:
+          filename:  enough of the name of the file where your 
+                     requested cutout originates that we can uniquely glob it; 
+                     a UTC string, for example.
+          extnumber: an extension number (corresponding to the order in which this file 
+                     was stored in the cookie cutter. Note that this does NOT correspond
+                     to the actual order of the fits extensions in the raw file.
+        ---------------
+        Optional:
+        cutoutType: defaults to IMAGE; may also provide MASK.
+        '''
+        
+        # Construct the file extension from the requested cutout:
+        
+        if (filename is None) and (extnumber is not None):
+            extnumber = int(extnumber) # Avoiding needless silliness
+            extname = f"{cutoutType}{extnumber}"
+            # Look up the row in the table.
+            
+        if (filename is not None) and (extnumber is None):
+            filematch = [filename in thing for thing in self._object_info_table['imagefile']]
+            if len(filematch) == 1:
+                extnumber = filematch.index(True)
+                extname = f"{cutoutType}{extnumber}"
+            else:
+                raise ValueError(f"the provided file string, {filename}, is not unique in the list of provided image files.")
+        if (filename is not None) and (extnumber is not None):
+            raise ValueError("should provide only the extension number or a unique fragment of the input filename, but not both")
+        if (filename is None) and (extnumber is None):
+            raise ValueError("should provide at least one of the extension number or a unique fragment of the input filename")
+        
+        # we should have arrived here with an extension name.
+        # Now get the row from the table with the object info.
+        # At this point, the extension name should exist.
+        row = (self._object_info_table['object index'] == objectid) & (self._object_info_table['extnumber'] == extnumber)
+        if np.sum(row) != 1:
+            raise ValueError(f"somehow, the object index {objectid} and image extension {extnumber} pairing does not exist, or is not unique in your metadata")
+        entry = self._object_info_table[row]
+        
+        with fitsio.FITS(self._cookieCutterFile,"r") as fits:
+            cutout1d = fits[extname][row[startpos]:row[endpos]]
+            # Note: cutouts are always square.
+            cutout2d = cutout1d.reshape(int(np.sqrtcutout1d.size),int(np.sqrtcutout1d.size))
+        return cutout2d
+        
         
