Fixed some bugs in interfacing new astrometry & masking code w/ OBA

superbit_lensing/oba/masking.py
@@ -3,7 +3,7 @@ from glob import glob
 import os
 import numpy as np
 import fitsio
-import lacosmic
+from lacosmic import lacosmic
 
 from superbit_lensing import utils
 from superbit_lensing.oba.oba_io import band2index
@@ -126,32 +126,39 @@ class MaskingRunner(object):
 
         return
 
-    def mask_cosmic_rays(self, logprint):
+    def mask_cosmic_rays(self, logprint, sci_ext=0):
         '''
-        TODO: Run a cosmic ray finder on each cal image and combine mask
+        Run a cosmic ray finder on each cal image and combine mask
         with self.masks entry
+
+        logprint: utils.LogPrint
+            A LogPrint instance for simultaneous logging & printing
+        sci_ext: int
+            The fits extension for the sci image
         '''
 
         for band in self.bands:
-            logprint(f'Starting cosmic Ray masking on band {band}')
+            logprint(f'Starting cosmic ray masking on band {band}')
 
             cal_dir = (self.run_dir / band / 'cal/').resolve()
             bindx = band2index(band)
 
-            cal_files = glob(
-                str(cal_dir / f'{self.target_name}*_{bindx}_*_cal.fits')
-                )
+            for cal_file in self.masks.keys():
+                logprint(f'Running lacosmic on {cal_file.name}')
 
-            for cal_file in cal_files:
-                data = fitsio.read(cal_file, ext=0)
-                data_cr_corr, cr_mask = lacosmic.lacosmic(data=data.astype(np.float32),
-                                                          contrast=2,
-                                                          cr_threshold=6,
-                                                          neighbor_threshold=6,
-                                                          effective_gain=0.343, # e-/ADU
-                                                          readnoise=2.08, # e- RMS
-                                                          maxiter=2)
-                
+                data = fitsio.read(str(cal_file), ext=sci_ext)
+
+                # TODO: can we move some of these pars to a config?
+                data_cr_corr, cr_mask = lacosmic(
+                    data=data.astype(np.float32),
+                    contrast=2,
+                    cr_threshold=6,
+                    neighbor_threshold=6,
+                    effective_gain=0.343, # e-/ADU
+                    readnoise=2.08, # e- RMS
+                    maxiter=2
+                    )
+
                 # AND cosmic ray mask with the existing mask on cal file
                 self.masks[cal_file] *= cr_mask
 