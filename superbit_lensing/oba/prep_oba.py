5cbba595981d9e7856bfbe2c62fd46ce13d4c3fa
Spencer Everett <spencerweverett@gmail.com>
2023-03-13 14:16:18 +1300

    Updated OBA scripts for QCC commander format

--- superbit_lensing/oba/prep_oba.py
+++ superbit_lensing/oba/prep_oba.py
@@ -1,14 +1,3 @@
-import shutil
-from pathlib import Path
-from argparse import ArgumentParser
-from glob import glob
-import fitsio
-
-from superbit_lensing import utils
-from oba_io import IOManager, band2index
-
-import ipdb
-
 '''
 This script is used for preparing the SuperBIT onboard analysis (OBA) on the
 QCC flight computer. It expects a target name and a series of integers for
@@ -25,6 +14,17 @@ header IMG_QUAL to one of ['GOOD', 'BAD', 'UNVERIFIED']. In the case that the
 image checker does not work or is not run on some images, you can choose to
 allow unverified images in the analysis
 '''
+
+import shutil
+from pathlib import Path
+from argparse import ArgumentParser
+from glob import glob
+import fitsio
+
+from superbit_lensing import utils
+from oba_io import IOManager, band2index
+
+import ipdb
 
 def parse_args():
 
@@ -44,15 +44,17 @@ def parse_args():
     parser.add_argument('req_r_images', type=int,
                         help='Number of required r-band images for the OBA run ' +
                         '(-1 to not analyze band during OBA)')
-    parser.add_argument('req_lum_images', type=int,
-                        help='Number of required lum-band images for the OBA run ' +
-                        '(-1 to not analyze band during OBA)')
     parser.add_argument('req_nir_images', type=int,
                         help='Number of required nir-band images for the OBA run ' +
                         '(-1 to not analyze band during OBA)')
-    parser.add_argument('allow_unverified', type=str, choices=['True', 'False'],
-                        help='Set to True to allow images not verified by the '
-                        'image checker to be analyzed by the OBA. Otherwise False')
+    parser.add_argument('req_lum_images', type=int,
+                        help='Number of required lum-band images for the OBA run ' +
+                        '(-1 to not analyze band during OBA)')
+
+    # NOTE: We define the bool arg this way to make it work cleanly with the QCC commander
+    parser.add_argument('allow_unverified', type=int, choices=[0, 1],
+                        help='Set to 1 (True) to allow images not verified by the '
+                        'image checker to be analyzed by the OBA. Otherwise 0 (False)')
 
     # NOTE: Only use this if you are testing this script locally!
     parser.add_argument('-root_dir', type=str, default=None,
@@ -71,18 +73,15 @@ def main(args):
 
     # annoying situation due to argparse rules + no optional args
     # for SB commander
-    if allow_unverified == 'True':
-        allow_unverified = True
-    elif allow_unverified == 'False':
-        allow_unverified = False
+    allow_unverified = bool(allow_unverified)
 
     req_images = {
         'u': args.req_u_images,
         'b': args.req_b_images,
         'g': args.req_g_images,
         'r': args.req_r_images,
-        'lum': args.req_lum_images,
         'nir': args.req_nir_images,
+        'lum': args.req_lum_images,
     }
 
     # this will keep track of how many acceptable images are in RAW_DIR
@@ -102,14 +101,13 @@ def main(args):
     # For each band, collect all images in RAW_DATA and determine if
     # each is valid for OBA
 
+    failed = False
     for band in bands:
-        print(f'Checking band {band}')
         if req_images[band] == -1:
             # this sentinel value indicates that the band should be
             # ignored for the OBA of this target
             use_bands.remove(band)
             acceptable_images[band] = 0
-            print(f'Ignoring {band} as required images set to -1')
             continue
 
         bindx = band2index(band)
@@ -117,8 +115,6 @@ def main(args):
             io_manager.RAW_DATA / f'{target_name}*_{bindx}_*.fits.bz2'
             )
         images = glob(search)
-
-        print(f'Found {len(images)} to check')
 
         good_images = []
         for image in images:
@@ -128,8 +124,9 @@ def main(args):
                 image_quality = hdr['IMG_QUAL']
             except KeyError:
                 # this shouldn't happen, but just in case
-                raise KeyError('SuperBIT SCI images should have IMG_QUAL in ' +
-                               'the FITS image header!')
+                print('SuperBIT SCI images should have IMG_QUAL in ' +
+                      'the FITS image header!')
+                return 1
 
             if image_quality == 'GOOD':
                 good_images.append(image)
@@ -138,13 +135,27 @@ def main(args):
                     good_images.append(image)
 
         Ngood = len(good_images)
-        if Ngood < req_images[band]:
-            print(f'You requested {req_images[band]} images for band {band} ' +
-                  f'but only {Ngood} were found using allow_unverified=' +
-                  f'{allow_unverified}')
-            return
-
         acceptable_images[band] = len(good_images)
+
+        if Ngood < req_images[band]:
+            failed = True
+
+    #-----------------------------------------------------------------
+    # Check if the number of acceptables images satisfies requirements
+
+    base_msg = 'Acceptable images:'
+    for band in bands:
+        if band in acceptable_images:
+            Nimages = acceptable_images[band]
+        else:
+            Nimages = -1
+        base_msg += f' {Nimages}'
+
+    if failed is True:
+        print(f'Failed: {base_msg}')
+        return 1
+    else:
+        print(base_msg)
 
     #-----------------------------------------------------------------
     # If the script has not ended yet, then the number of images requirements
@@ -170,32 +181,9 @@ def main(args):
         )
     utils.write_yaml(config, config_outfile)
 
-    #-----------------------------------------------------------------
-    # Now print out the config for the user to see & confirm
+    print(f'Config at {config_outfile}')
 
-    print('OBA prepper completed succesfully')
-    print('The following acceptable images were found for each desired band ' +
-          f'using allow_unverified={allow_unverified}:')
-
-    for band in use_bands:
-        Ngood = acceptable_images[band]
-        print(f'{band}: {Ngood}')
-
-    print(f'The following OBA configuration for {target_name} has been saved ' +
-          f'to: {config_outfile}')
-
-    for outer_key in config:
-        print(f'{outer_key}:')
-        if isinstance(config[outer_key], dict):
-            for inner_key, val in config[outer_key].items():
-                print(f'  {inner_key}: {val}')
-        elif isinstance(config[outer_key], list):
-            for item in config[outer_key]:
-                print(f'  {item}')
-        else:
-            print(config[outer_key])
-
-    return
+    return 0
 
 if __name__ == '__main__':
     args = parse_args()
--- superbit_lensing/oba/update_oba_global_config.py
+++ superbit_lensing/oba/update_oba_global_config.py
@@ -1,3 +1,35 @@
+'''
+This script is used for updating the SuperBIT onboard analysis (OBA) global
+config file using the QCC commander. It only updates one config value at a
+time. As each config field (besides `modules`) is a 2-level dict, we *would*
+use the following scheme:
+
+python update_oba_global_config.py outer_key, inner_key, new_val, val_type
+
+*HOWEVER*, the QCC commander can only pass 1 string. As all of these inputs
+need to be strings (and cannot use commas), we do the following:
+
+python update_oba_global_config.py "outer_key&inner_key&new_val&val_type"
+
+where:
+
+- outer_key: Name of the outer OBA config key
+- inner_key: Name of the inner OBA config key
+- new_val: The new value for the OBA global config entry
+- val_type: The explicit type that you want to save the new config value as
+            in the config
+
+NOTE: ".", "_", "-", etc. are not acceptable delimiters as they may be part of
+the new value you are trying to construct
+
+NOTE: to pass a list or tuple of str's, do something like the following:
+
+python update_oba_global_config.py outer_key&inner_key&['val1', 'val2']&list
+
+output:
+    print(outer_key: [inner_key: old_val -> type(new_val)])
+'''
+
 import shutil
 from pathlib import Path
 from argparse import ArgumentParser
@@ -8,31 +40,13 @@ from superbit_lensing import utils
 
 import ipdb
 
-'''
-This script is used for updating the SuperBIT onboard analysis (OBA) global
-config file using the QCC commander. It only updates one config value at a
-time. As each config field (besides `modules`) is a 2-level dict, we use
-the following scheme:
-
-python update_oba_global_config.py outer_key, inner_key, new_val, val_type
-
-outer_key:
-  inner_key: old_val -> type(new_val)
-'''
-
 def parse_args():
 
     parser = ArgumentParser()
 
-    parser.add_argument('outer_key', type=str,
-                        help='Name of the outer OBA config key')
-    parser.add_argument('inner_key', type=str,
-                        help='Name of the inner OBA config key')
-    parser.add_argument('new_val', type=str,
-                        help='The new value for the OBA global config entry')
-    parser.add_argument('val_type', type=str,
-                        help='The explicit type that you want to save the ' +
-                        'new config value as in the config')
+    parser.add_argument('update_tokens', type=str,
+                        help='The update tokens in the format of: ' +
+                        'outer_key.inner_key.new_val.val_type')
 
     return parser.parse_args()
 
@@ -41,10 +55,18 @@ def main(args):
     #-----------------------------------------------------------------
     # Initial setup
 
-    outer_key = args.outer_key
-    inner_key = args.inner_key
-    new_val = args.new_val
-    val_type = args.val_type
+    update_tokens = args.update_tokens
+    tokens = update_tokens.split('&')
+
+    if len(tokens) != 4:
+        print('Failed: The input must have the format of: ' +
+              'outer_key&inner_key&new_val&val_type')
+        return 1
+
+    outer_key = tokens[0]
+    inner_key = tokens[1]
+    new_val = tokens[2]
+    val_type = tokens[3]
 
     #-----------------------------------------------------------------
     # Load in existing global OBA config
@@ -64,22 +86,27 @@ def main(args):
             # this only works for an empty str
             new_val = ''
         else:
-            raise ValueError('If val_type is bool, can only pass one of the ' +
-                             'following: [True, False] (in any capitalization)')
+            print('Failed: For val_type == bool, can only pass one of the ' +
+                  'following: [True, False] (in any capitalization)')
+            return 1
 
     #-----------------------------------------------------------------
     # this grabs the relevant type operator for proper type casting
-    casted_val = getattr(__builtins__, val_type)(new_val)
+
+    if val_type in ['list', 'tuple']:
+        import ast
+        casted_val = ast.literal_eval(new_val)
+    else:
+        casted_val = getattr(__builtins__, val_type)(new_val)
+
     global_config[outer_key][inner_key] = casted_val
 
     utils.write_yaml(global_config, global_config_file)
 
-    print('Made the following update to the OBA global config:')
-    print(f'{outer_key}:')
-    print(f'  {inner_key}: {old_val} -> {casted_val}')
+    print(f'{outer_key}: [{inner_key}: {old_val} -> {casted_val}]')
 
-    return
+    return 0
 
 if __name__ == '__main__':
     args = parse_args()
-    main(args)
+    rc = main(args)
