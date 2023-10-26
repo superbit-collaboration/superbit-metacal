21a0b138920bdf6b3830c588f0b04ebd55298d6a
Georgios <vassilakis.g@northeastern.edu>
2023-10-26 16:33:43 -0400

    update hotcold_sextractor.py

--- superbit_lensing/medsmaker/scripts/hotcold_sextractor.py
+++ superbit_lensing/medsmaker/scripts/hotcold_sextractor.py
@@ -2,7 +2,7 @@ import os
 import numpy as np
 import yaml
 import astropy.io.fits as fits
-from astropy.table import Table
+from astropy.table import Table, vstack
 from astropy.coordinates import SkyCoord
 from shapely.geometry.polygon import Polygon
 from shapely.geometry import MultiPolygon, box
@@ -32,7 +32,7 @@ class HotColdSExtractor:
         self.merged_data = []
         self.exclusion_zones = []
 
-    def run(self, imagefile):
+    def run(self, imagefile, outdir):
         '''
         Function that calls the SExtractor command building function based on the selected mode.
         '''
@@ -54,14 +54,25 @@ class HotColdSExtractor:
             print(f"Default mode catalog complete: {default_cat}")
 
         # Check if the catalogs exist before merging
-        if cold_cat and hot_cat and os.path.exists(cold_cat) and os.path.exists(hot_cat):
-            # Define output catalog name
-            base_name = os.path.basename(imagefile)
-            outname = base_name.replace('.fits', '_cat' + '_merged'+'.fits')
+        if os.path.exists(cold_cat) and os.path.exists(hot_cat):
+
+            # Define output catalog names and outdirs
+            if "det/cat" in self.outdir:
+                base_name = os.path.basename(self.coadd_file)
+                outname = base_name.replace('.fits', '_det_cat.fits')
+            else:
+                base_name = os.path.basename(imagefile)
+                outname = base_name.replace('.fits', '_cat' +'.fits')  
+
             # Merge the catalogs
             print(f"Merging catalogs {cold_cat} and {hot_cat}")
             self._merge_catalogs(cold_cat, hot_cat, self.buffer_radius, self.n_neighbors, outname)
             print(f"Merged catalog complete: {outname}")
+
+            # Delete the hot and cold catalogs after merging
+            os.remove(hot_cat)
+            os.remove(cold_cat)
+            print(f"Deleted catalogs: {hot_cat} and {cold_cat}")
         else:
             raise FileNotFoundError(f"One or both of the catalogs {cold_cat}, {hot_cat} do not exist, merged catalog NOT created.")
 
@@ -80,10 +91,7 @@ class HotColdSExtractor:
         base_name = os.path.basename(image_file)
 
         # Different output catalog nomenclature for coadd versus single exposure
-        if "det/cat" in outdir:
-            cat_name = base_name.replace('.fits', '_det_cat.fits')
-        else:
-            cat_name = base_name.replace('.fits', '_cat' + f'_{mode}'+'.fits')
+        cat_name = base_name.replace('.fits', '_cat' + f'_{mode}'+'.fits')
 
         cat_file = os.path.join(cat_dir, cat_name)
 
@@ -113,7 +121,7 @@ class HotColdSExtractor:
         self.outdir = os.path.join(self.data_dir, self.target_name, self.band, "cat")
 
         for imagefile in self.image_files:
-            sexcat = self.run(imagefile)
+            sexcat = self.run(imagefile, self.outdir)
             exposure_catalogs.append(sexcat)
 
         return exposure_catalogs
@@ -125,7 +133,7 @@ class HotColdSExtractor:
         '''
         self.coadd_file = os.path.join(self.data_dir, self.target_name, "det", "coadd", f"{self.target_name}_coadd.fits")
         self.outdir = os.path.join(self.data_dir, self.target_name, "det", "cat")
-        self.run(self.coadd_file)
+        self.run(self.coadd_file, self.outdir)
 
 
     def _construct_sextractor_cmd(self, image, cat_file, cpath, mode):
@@ -141,7 +149,7 @@ class HotColdSExtractor:
         name_arg =              f'-CATALOG_NAME {cat_file}'
         param_arg =             f'-PARAMETERS_NAME {os.path.join(cpath, "sextractor.param")}'
         nnw_arg =               f'-STARNNW_NAME {os.path.join(cpath, "default.nnw")}'
-        checktype_arg =         f'-CHECKIMAGE_TYPE -BACKGROUND,SEGMENTATION,APERTURES'
+        checktype_arg =         f'-CHECKIMAGE_TYPE -BACKGROUND,SEGMENTATION'
 
         bkg_name =              image.replace('.fits','.sub.fits')
         seg_name =              image.replace('.fits','.sgm.fits')
@@ -160,7 +168,7 @@ class HotColdSExtractor:
             filter_arg =        f'-FILTER_NAME {os.path.join(cpath, "default.conv")}'
             aper_name =         os.path.join(self.outdir, f"{aper_name_base}.default.fits")
 
-        checkname_arg =         f'-CHECKIMAGE_NAME {bkg_name},{seg_name},{aper_name}'
+        checkname_arg =         f'-CHECKIMAGE_NAME {bkg_name},{seg_name}'
 
         # Make the SExtractor command
         cmd = ' '.join([
@@ -195,27 +203,27 @@ class HotColdSExtractor:
 
         # Load in hot and cold catalogs
         with fits.open(cold_cat) as hdul1:
-            cold_data = hdul1[1].data
+            cold_data = hdul1[2].data
         with fits.open(hot_cat) as hdul2:
-            hot_data = hdul2[1].data
+            hot_data = hdul2[2].data
 
         cold_idx = index.Index()
         buffer_radius = self.buffer_radius
         n_neighbors = self.n_neighbors
 
         # Get the Kron minimum radius from the cold catalog
-        kron_min_radius_cold = self.get_kron_min_radius(cold_cat)
-        kron_min_radius_hot = self.get_kron_min_radius(hot_cat)
+        kron_min_radius_cold = 3.0
+        kron_min_radius_hot = 3.0
 
         print(f'Creating ellipses for {len(cold_data)} cold sources')
         # Loop over each source in the cold catalog
         for i, cold_source in enumerate(cold_data):
             # Convert the cold source parameters to a SkyCoord object
             cold_coord = SkyCoord(cold_source['ALPHAWIN_J2000'], cold_source['DELTAWIN_J2000'], unit='deg')
-
+            
             # Create an ellipse for the cold source
             try:
-                ellipse = self.create_ellipse(cold_coord.ra.deg, cold_coord.dec.deg, cold_source['A_WORLD']*kron_min_radius_cold,
+                ellipse = self.create_ellipse(cold_coord.ra.deg, cold_coord.dec.deg, cold_source['A_WORLD']*kron_min_radius_cold, 
                                 cold_source['B_WORLD']*kron_min_radius_cold, cold_source['THETA_IMAGE'])
                 # Add a check here to see if the ellipse was created successfully
                 if ellipse is None:
@@ -235,14 +243,17 @@ class HotColdSExtractor:
 
         print(f'Creating ellipses for {len(hot_data)} hot sources')
         # Loop over each source in the hot catalog
-        for hot_source in hot_data:
+        # Initialize a boolean array with the same length as hot_data and set all values to False
+        wg = np.full(len(hot_data), False)
+
+        for idx, hot_source in enumerate(hot_data):
             # Convert the hot source parameters to a SkyCoord object
             hot_coord = SkyCoord(hot_source['ALPHAWIN_J2000'], hot_source['DELTAWIN_J2000'], unit='deg')
 
             # Create an ellipse for the hot source
             try:
                 hot_object = self.create_ellipse(hot_coord.ra.deg, hot_coord.dec.deg, hot_source['A_WORLD']*kron_min_radius_hot,
-                                            hot_source['B_WORLD']*kron_min_radius_hot, hot_source['THETA_IMAGE'])
+                                                hot_source['B_WORLD']*kron_min_radius_hot, hot_source['THETA_IMAGE'])
                 # Add a check here to see if the hot object was created successfully
                 if hot_object is None:
                     continue
@@ -257,14 +268,27 @@ class HotColdSExtractor:
 
             # Get the list of 5 nearest cold sources
             nearest_sources_ids = list(cold_idx.nearest(hot_buffer_bounds.bounds, n_neighbors))
-
+            
             # Get the actual nearest cold sources
             nearest_sources = [self.exclusion_zones[i] for i in nearest_sources_ids if 0 <= i < len(self.exclusion_zones)]
-
+            
             if not any(source.contains(hot_object) or source.intersects(hot_object) for source in nearest_sources):
-                self.merged_data.append(hot_source)
+                wg[idx] = True
+
         print('Hot sources referenced to exclusion zone, merging catalogs')
-        # Save the merged catalog:
-        merged_table = Table(rows=self.merged_data)
-        hdu = fits.BinTableHDU(merged_table)
-        hdu.writeto(os.path.join(self.outdir, outname), overwrite=True)
+
+        # Merge catalogs
+        cold_fits_base = fits.open(cold_cat)
+
+        # Add header comment noting that it is a merged catalog
+        cold_fits_base[0].header['COMMENT'] = 'Hot/Cold used for this catalog'
+
+        cold_table = Table.read(cold_cat, hdu=2)
+        hot_table = Table.read(hot_cat, hdu=2) 
+
+        merged_table = vstack([cold_table, hot_table[wg]])
+
+        cold_fits_base[2].data = merged_table.as_array()
+
+        # Write the merged catalog to a FITS file
+        cold_fits_base.writeto(os.path.join(self.outdir, outname), overwrite=True)
