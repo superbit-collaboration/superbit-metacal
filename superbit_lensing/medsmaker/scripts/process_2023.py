18d673b50cc68ae5fabf268bf853266333da98fc
Jacqueline McCleary <jmac.ftw@gmail.com>
2024-05-17 00:37:21 -0400

    Update process_2023.py
    Remove [0:3]!
    
    Signed-off-by: Jacqueline McCleary <jmac.ftw@gmail.com>

--- superbit_lensing/medsmaker/scripts/process_2023.py
+++ superbit_lensing/medsmaker/scripts/process_2023.py
@@ -105,7 +105,7 @@ def main(args):
         for ending in endings:
             search_path = os.path.join(data_dir, target_name, band, 'cal', f'*{ending}.fits')
             science.extend(glob(search_path))   
-        science=science[0:3]
+            
         logprint(f'\nUsing science frames: {science}\n')
 
         # Define output MEDS name
